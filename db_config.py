b9a1cece69c539846c077d0dc78425665e665b04
Shahid-Dev <shahidsiddhiki845@gmail.com>
2026-04-23 08:11:01 +0530

    Update database connection credentials in db_config.py

--- db_config.py
+++ db_config.py
@@ -3,11 +3,10 @@ from pymysql.err import MySQLError
 
 def create_database(d_name):
     try:
-        con=pymysql.connect(host='localhost', user='txn_user', password='Shahid!789')
+        con=pymysql.connect(host='localhost', user='Your_user_name', password='Your_Password')
         cur=con.cursor()
         cur.execute(f"CREATE DATABASE IF NOT EXISTS {d_name}")
         con.commit()
-        #print(f"Databse {DB_NAME} ensured.")
     except MySQLError as e:
         print(f"Error creating database: {e}")
     finally:
@@ -16,7 +15,7 @@ def connect_to_db():
     DB_NAME="transaction_db".strip().lower()
     create_database(DB_NAME)
     try:
-        con=pymysql.connect(host="localhost", user="txn_user", password="Shahid!789",database=DB_NAME)
+        con=pymysql.connect(host="localhost", user='Your_user_name', password='Your_Password',database=DB_NAME)
         return con
     except MySQLError as e:
         print(f"Error connecting to database:",e)
